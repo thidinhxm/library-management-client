d917058b6bf2a437685f62694896272c5ea86968
thidinhxm <thidinhxm@gmail.com>
2022-05-12 09:58:14 +0700

    setup project

---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import homeStyle from './home.module.scss'
+
+const Home = () => {
+  return (
+    <div className={homeStyle.home}>
+      <h2 className={homeStyle.title}>HOME PAGE</h2>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/login/Login.js
@@ -0,0 +1,9 @@
+import loginStyle from './login.module.scss'
+
+const Login = () => {
+  return (
+    <div className={loginStyle.login}>Login Page</div>
+  )
+}
+
+export default Login
