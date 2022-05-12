c6731717d18a0ce213dd704e4b90b0576a94328e
thidinhxm <thidinhxm@gmail.com>
2022-05-12 11:41:30 +0700

    login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,23 @@ import loginStyle from './login.module.scss'
 
 const Login = () => {
   return (
-    <div className={loginStyle.login}>Login Page</div>
+    <div className={loginStyle.login}>
+      <form className={loginStyle.loginForm}>
+        <h1 className={loginStyle.title}>Đăng nhập</h1>
+
+        <div className={loginStyle.inputContainer}>
+          <input type="text" placeholder="Email" className={loginStyle.input}/>
+          <label className={loginStyle.label}>Email</label>
+        </div>
+
+        <div className={loginStyle.inputContainer}>
+          <input type="text" placeholder="Password" className={loginStyle.input}/>
+          <label className={loginStyle.label}>Mật khẩu</label>
+        </div>
+
+        <button className={loginStyle.btnSubmit}>Đăng nhập</button>
+      </form>
+    </div>
   )
 }
 
